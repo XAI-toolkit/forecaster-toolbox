2673e1d02d3326bc7efe9dd2273e5ab0cb2c1c0a
Dimitris Tsoukalas <tsoukj@iti.gr>
2020-04-08 18:49:34 +0300

    Added more service tests

--- tests/test_forecaster_service.py
+++ tests/test_forecaster_service.py
@@ -5,8 +5,22 @@
 
 import requests
 
-def test_td_forecasting():
-    url = 'http://160.40.51.206:5000/ForecasterToolbox/TDForecasting?horizon=5&project=apache_kafka_measures&regressor=auto&ground_truth=no&test=no'
+def test_td_forecasting_status_code():
+    url = 'http://localhost:5000/ForecasterToolbox/TDForecasting?horizon=5&project=apache_kafka_measures&regressor=auto&ground_truth=no&test=no'
+
+    r = requests.get(url)
+
+    assert r.status_code == 200
+
+def test_energy_forecasting_status_code():
+    url = 'http://localhost:5000/ForecasterToolbox/EnergyForecasting?horizon=5&project=sbeamer_gapbs_energy_measures&regressor=auto&ground_truth=no&test=no'
+
+    r = requests.get(url)
+
+    assert r.status_code == 200
+
+def test_dependability_forecasting_status_code():
+    url = 'http://localhost:5000/ForecasterToolbox/DependabilityForecasting?horizon=5&project=square_retrofit_security_measures&regressor=auto&ground_truth=no&test=no'
 
     r = requests.get(url)
 
