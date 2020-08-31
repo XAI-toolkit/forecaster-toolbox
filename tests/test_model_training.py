08421bca49a6e1085fc0892b5402f0809cfda4d6
Dimitris Tsoukalas <tsoukj@iti.gr>
2020-08-31 12:52:15 +0300

    Increased test coverage for 'model_training' file

--- tests/test_model_training.py
+++ tests/test_model_training.py
@@ -7,7 +7,7 @@ from pmdarima import ARIMA
 
 import sys
 sys.path.append('..')
-from model_training import grid_search_best, arima_search_best, cross_validation_best, create_regressor
+from model_training import grid_search_best, arima_search_best, cross_validation_best, create_regressor, build_and_train_td, build_and_train_dependability, build_and_train_energy, build_and_train_td_class_level
 
 def test_grid_search_best_lasso(x_array_input, y_array_input):
     assert 'regressor__alpha' in grid_search_best('lasso', x_array_input, y_array_input)
@@ -48,5 +48,79 @@ def test_create_regressor_svr_rbf(x_array_input, y_array_input):
 def test_create_regressor_random_forest(x_array_input, y_array_input):
     assert create_regressor('random_forest', x_array_input, y_array_input)['regressor'] is not None
 
+def test_create_regressor_arima(x_array_input, y_array_input):
+    assert create_regressor('arima', x_array_input, y_array_input) is not None
+
 def test_create_regressor_auto(x_array_input, y_array_input):
     assert create_regressor('auto', x_array_input, y_array_input)['regressor'] is not None
+
+def test_build_and_train_td(forecasting_project_input, forecasting_horizon_input):
+    result = build_and_train_td(forecasting_horizon_input, forecasting_project_input, 'mlr', 'yes', 'yes')
+    assert 'parameters' in result
+    assert 'forecasts' in result
+    assert 'ground_truth' in result
+    assert len(result['forecasts']) == forecasting_horizon_input
+
+def test_build_and_train_td_no_test_param(forecasting_project_input, forecasting_horizon_input):
+    result = build_and_train_td(forecasting_horizon_input, forecasting_project_input, 'mlr', 'yes', 'no')
+    assert 'parameters' in result
+    assert 'forecasts' in result
+    assert 'ground_truth' in result
+    assert len(result['forecasts']) == forecasting_horizon_input
+
+# def test_build_and_train_td_arima(forecasting_project_input, forecasting_horizon_input):
+#     result = build_and_train_td(forecasting_horizon_input, forecasting_project_input, 'arima', 'yes', 'no')
+#     assert 'parameters' in result
+#     assert 'forecasts' in result
+#     assert 'ground_truth' in result
+#     assert len(result['forecasts']) == forecasting_horizon_input
+
+def test_build_and_train_td_class_level(forecasting_project_class_level_input, forecasting_horizon_input, forecasting_class_number_input):
+    result = build_and_train_td_class_level(forecasting_horizon_input, forecasting_project_class_level_input, forecasting_class_number_input, 'mlr', 'yes', 'no')
+    assert 'parameters' in result
+    assert 'change_metrics' in result
+    assert 'forecasts' in result
+    assert 'ground_truth' in result
+    assert len(result['forecasts']) == forecasting_horizon_input
+
+def test_build_and_train_dependability(forecasting_project_input, forecasting_horizon_input):
+    result = build_and_train_dependability(forecasting_horizon_input, forecasting_project_input, 'mlr', 'yes', 'yes')
+    assert 'parameters' in result
+    assert 'forecasts' in result
+    assert 'ground_truth' in result
+    assert len(result['forecasts']) == forecasting_horizon_input
+
+def test_build_and_train_dependability_no_test_param(forecasting_project_input, forecasting_horizon_input):
+    result = build_and_train_dependability(forecasting_horizon_input, forecasting_project_input, 'mlr', 'yes', 'no')
+    assert 'parameters' in result
+    assert 'forecasts' in result
+    assert 'ground_truth' in result
+    assert len(result['forecasts']) == forecasting_horizon_input
+
+def test_build_and_train_dependability_arima(forecasting_project_input, forecasting_horizon_input):
+    result = build_and_train_dependability(forecasting_horizon_input, forecasting_project_input, 'arima', 'yes', 'no')
+    assert 'parameters' in result
+    assert 'forecasts' in result
+    assert 'ground_truth' in result
+    assert len(result['forecasts']) == forecasting_horizon_input
+
+def test_build_and_train_energy(forecasting_project_input, forecasting_horizon_input):
+    result = build_and_train_energy(forecasting_horizon_input, forecasting_project_input, 'mlr', 'yes', 'yes')
+    assert 'parameters' in result
+    assert 'forecasts' in result
+    assert 'ground_truth' in result
+    assert len(result['forecasts']) == forecasting_horizon_input
+
+def test_build_and_train_energy_no_test_param(forecasting_project_input, forecasting_horizon_input):
+    result = build_and_train_energy(forecasting_horizon_input, forecasting_project_input, 'mlr', 'yes', 'no')
+    assert 'parameters' in result
+    assert 'forecasts' in result
+    assert 'ground_truth' in result
+    assert len(result['forecasts']) == forecasting_horizon_input
+
+def test_build_and_train_energy_arima(forecasting_project_input, forecasting_horizon_input):
+    result = build_and_train_energy(forecasting_horizon_input, forecasting_project_input, 'arima', 'yes', 'no')
+    assert 'parameters' in result
+    assert 'forecasts' in result
+    assert 'ground_truth' in result
+    assert len(result['forecasts']) == forecasting_horizon_input
